69be29df56e1b1f5f25770de68ad690e83142bb1
William Nelson <swilliam.nelson@gmail.com>
2019-06-05 16:10:29 +0200

    Faites pas attention c'est juste ma dernière version

--- PLA_GRP80/MyProject/src/mesProto/Wall.java
+++ PLA_GRP80/MyProject/src/mesProto/Wall.java
@@ -1,5 +1,25 @@
 package mesProto;
 
-public class Wall {
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+public class Wall extends Component{
+	
+	int m_idx;
+	
+	Wall(Model model, int no, BufferedImage sprite, int rows, int columns, int x, int y, float scale){
+		super(model, no, sprite, rows, columns, x, y, scale);
+		m_idx = 0;
+		splitSprite();
+	}
+	
+	@Override
+	void paint(Graphics g) {
+	    Image img = m_sprites[m_idx];
+	    int w = (int)(m_scale * m_w);
+	    int h = (int)(m_scale * m_h);
+	    g.drawImage(img, m_x, m_y, w, h, null);
+	}
 
 }
